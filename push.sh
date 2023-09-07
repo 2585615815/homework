8fe75ee2d96e523cfa17693ecda8c233a7ef7dd5
2585615815 <2585615815@qq.com>
2023-09-07 11:59:15 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-git config--global user.name'2585615815'
-git config--global user.email'2585615815@qq.com'
-git config--global http.sslverify false
-git add.
-git commit-m'自动推送'
+git config --global user.name '2585615815'
+git config --global user.email '2585615815@qq.com'
+git config --global http.sslverify false
+git add .
+git commit -m '自动推送'
 git push
